2b5d86a520d41ef584ece50dd82ee3b1810c9ae7
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-27 10:41:59 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,32 +2,6 @@
 import scraperwiki
 import urlparse
 import lxml.html
-import smtplib
-
-SERVER = "localhost"
-
-FROM = "sender@example.com"
-TO = ["user@example.com"] # must be a list
-
-SUBJECT = "Hello!"
-
-TEXT = "This message was sent with Python's smtplib."
-
-# Prepare actual message
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-# Send the mail
-
-server = smtplib.SMTP(SERVER)
-server.sendmail(FROM, TO, message)
-server.quit()
 
 
 # create a new function, which gets passed a variable we're going to call 'url'
