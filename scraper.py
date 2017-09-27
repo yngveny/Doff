3268778a0c52681e2066fb5a55f3ff9368cec183
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-27 10:30:58 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ msg.attach(MIMEText(body, 'plain'))
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "yuhjnm89")
+server.login(fromaddr, "Admiral01")
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
